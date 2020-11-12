1d26d9f62fd66e6b86e35751f7ec67f453b6564a
Manny Cortez Studios <emailme@mannycortez.com>
2020-11-12 14:15:05 -0800

    Added material-ui nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
 import Navbar from '../src/components/Navbar'
+import { Button } from '@material-ui/core'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Button variant='contained' color='primary'>
+        Hello World
+      </Button>
+      <Button variant='contained' color='link'>
+        Hello World 2
+      </Button>
+      
       <h1>Hello World</h1>
     </div>
   );
 }
+
+
 
 export default App;
